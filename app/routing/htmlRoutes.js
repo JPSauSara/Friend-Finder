1a8f5bf24669970d2d10a4cf8f937f5de5ad2486
JPSauSara <jpsausara@users.noreply.github.com>
2017-05-06 00:14:26 -0500

    fix: path

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,11 +8,11 @@ var path = require('path');
 module.exports = function (app) {
 
 	app.get('/survey', function(req, res) {
-		res.sendFile(path.join(__dirname + '../public/survey.html'))
+		res.sendFile(path.join(__dirname + '/../public/survey.html'))
 	});
 	// sends user to homepage
 	app.use('/', function(req, res) {
-		res.sendFile(path.join(__dirname + '../public/home.html'))
+		res.sendFile(path.join(__dirname + '/../public/home.html'))
 	});
 
 }
