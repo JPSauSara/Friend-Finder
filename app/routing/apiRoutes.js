6d1a057b420609de4360a77a4de13714fad86791
JPSauSara <jpsausara@users.noreply.github.com>
2017-05-06 09:22:26 -0500

    works: dropdown & api post

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,4 +12,13 @@ module.exports = function (app) {
 	app.get('/api/friends', function(req, res) {
 		res.json(friendsData);
 	});
-};
+
+
+	app.post('/api/friends', function(req, res){
+		// if(friendsData.length<5) {
+			friendsData.push(req.body);
+			res.json(true);
+		// }
+	});
+
+}
